6708bf0526165127ec31bc0cba7d1a4585c33adc
jackeyGao <gaojunqi@outlook.com>
2016-09-28 18:10:41 +0800

    [UPDATE] 增加下拉加载更多

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,14 @@
 //index.js
 //获取应用实例
 
-var feaUrl = "https://unsplash.com/napi/photos/curated?page=1&per_page=24&order_by=latest"
-var newUrl = "https://unsplash.com/napi/photos?page=1&per_page=24&order_by=latest"
+var feaUrl = "https://unsplash.com/napi/photos/curated"
+var newUrl = "https://unsplash.com/napi/photos"
 var cliendID = "d69927c7ea5c770fa2ce9a2f1e3589bd896454f7068f689d8e41a25b54fa6042"
 
-function fetchData (url, onSuccess) {
+function fetchData (url, params, onSuccess) {
   wx.request({
     url: url,
+    data: params,
     header: {
       "authorization": "Client-ID " + cliendID
     },
@@ -18,11 +19,12 @@ function fetchData (url, onSuccess) {
 var app = getApp()
 Page({
   data: {
-    latests: {},
+    latests: [],
     modalHidden: true,
     user: {},
     FeaActived: false,
-    NewActived: false
+    NewActived: false,
+    page: 1
   },
   modalTap: function(e) {
     var index = e.target.dataset.item
@@ -33,7 +35,12 @@ Page({
   },
   loadNewList: function(e) {
     var that = this
-    fetchData(newUrl, function (res) {
+    var params = {
+      page: 1,
+      per_page: 24,
+      order_by: 'latest'
+    }
+    fetchData(newUrl, params, function (res) {
       that.setData({
         latests: res.data,
         FeaActived: false,
@@ -43,11 +50,38 @@ Page({
   },
   loadFeaList: function() {
     var that = this
-    fetchData(feaUrl, function (res) {
+    var params = {
+      page: 1,
+      per_page: 24,
+      order_by: 'latest'
+    }
+    fetchData(feaUrl, params, function (res) {
       that.setData({
         latests: res.data,
         FeaActived: true,
         NewActived: false
+      })
+    })
+  },
+  loadMore: function(e) {
+    var that = this
+    that.setData({
+      page: that.data.page + 1
+    })
+    var params = {
+      page: this.data.page,
+      per_page: 12,
+      order_by: 'latest'
+    }
+
+    if (that.data.FeaActived) {
+      var url = feaUrl
+    } else {
+      var url = newUrl
+    }
+    fetchData(url, params, function (res) {
+      that.setData({
+        latests: that.data.latests.concat(res.data),
       })
     })
   },
